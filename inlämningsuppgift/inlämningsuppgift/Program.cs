b1805b3125b1a62efee95813b2e7fb03a9a3d9f3
Adam Wickström <adwi02002@utb.vaxjo.se>
2019-09-06 09:47:25 +0200

    kopierade eddie

---
+++ inlämningsuppgift/inlämningsuppgift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace inlämningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random generator = new Random();
+            int rand = generator.Next(1, 100);
+            Console.WriteLine(rand);
+            Console.WriteLine("gissa ett tal mellan 1 och 100!: ");
+            int svar = Console.ReadLine();
+            if (svar == rand) ;
+        }
+    }
+}
