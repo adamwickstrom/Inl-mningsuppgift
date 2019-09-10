47df52129aa9da66877b1b2d8e4a26752f2ce962
Adam Wickström <adwi02002@utb.vaxjo.se>
2019-09-10 13:04:00 +0200

    Kollade på Flashback och löste uppgiften

--- inlämningsuppgift/inlämningsuppgift/Program.cs
+++ inlämningsuppgift/inlämningsuppgift/Program.cs
@@ -8,11 +8,33 @@ namespace inlämningsuppgift
         {
 
             Random generator = new Random();
-            int rand = generator.Next(1, 100);
-            Console.WriteLine(rand);
+            int rand = generator.Next(1, 100), gissningar = 0;
             Console.WriteLine("gissa ett tal mellan 1 och 100!: ");
-            int svar = Console.ReadLine();
-            if (svar == rand) ;
-        }
+            int tal = Convert.ToInt32(Console.ReadLine());
+            while (tal != rand)
+            {
+                if (tal < rand) 
+
+
+                {
+                    Console.WriteLine("För lågt!");
+                }
+
+                else if (tal > rand) 
+
+
+                {
+                    Console.WriteLine("för stort!");
+                }
+
+                tal = Convert.ToInt32(Console.ReadLine());
+                gissningar++;
+
+            }
+
+            Console.WriteLine("Du gissade rätt!");
+            Console.WriteLine("Du gissade " + gissningar + " gånger");
+        }   
+
     }
 }
