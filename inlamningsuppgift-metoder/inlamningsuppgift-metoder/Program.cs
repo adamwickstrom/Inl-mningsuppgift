63290c600292f7163ed89b74d4c1179914ba3371
Adam Wickström <adwi02002@utb.vaxjo.se>
2019-09-20 09:34:02 +0200

    gjorde allt

---
+++ inlamningsuppgift-metoder/inlamningsuppgift-metoder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace inlamningsuppgift_metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+        }
+
+
+        static double CirkelOmkrets(double radie)
+        {
+           return (2 * radie * 3.14);  
+        }
+        
+        static double CirkelArea(double radie)
+        {
+            return (radie * radie * 3.14);
+         }
+
+
+
+
+    }
+
+
+
+
+}
