1f2e1332439e384db69d2563644018d0a00091a5
hugobacher <huba02001@utb.vaxjo.se>
2020-01-17 13:27:56 +0100

    Gjorde allt på en lektion

---
+++ Inlämning bubblesort/Inlämning bubblesort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class MySort
+    {
+        static void Main(string[] args)
+        {
+            int[] size = { 100, 1000, 10000 };
+            int[] lista = randomNummer(size[1]);
+            BubbleSort(lista);
+        }
+        static void BubbleSort(int[] lista)
+        {
+            DateTime starttid = DateTime.Now;
+            for (int i = lista.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    if (lista[j] > lista[j + 1])
+                    {
+                        int high = lista[j];
+                        lista[j] = lista[j + 1];
+                        lista[j + 1] = high;
+                    }
+                }
+            }
+            TimeSpan tid = DateTime.Now - starttid;
+
+            for(int f = 0; f < lista.Length; f++)
+            {
+                Console.WriteLine(lista[f]);
+            }
+
+            Console.WriteLine($"{tid.TotalSeconds}s");
+        }
+        static int[] randomNummer(int size)
+        {
+            Random slump = new Random();
+            int[] nummer = new int[size];
+            for (int i = 0; i < nummer.Length; i++) {
+                nummer[i] = slump.Next(nummer.Length);
+            }
+            return nummer;          
+        }
+
+    }
+
+   
+}
